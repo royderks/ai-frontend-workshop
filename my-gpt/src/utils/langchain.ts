0735f133114152b0ce6060fd3c002881e480ff8d
Roy Derks <rmaeaderks@gmail.com>
2024-04-02 20:22:57 +0100

    Added ex5 solution

--- my-gpt/src/utils/langchain.ts
+++ my-gpt/src/utils/langchain.ts
@@ -1,17 +1,31 @@
 import { OpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
 
+/*
+  You are a naming consultant for new companies.
+  What is a good name for a company that makes colorful socks?
+*/
 const llm = new OpenAI({
     openAIApiKey: import.meta.env.VITE_OPENAI_KEY,
     temperature: 1
 });
 
-export async function generateAnswer(question: string) {
+export async function generateAnswer(
+    question: string,
+    promptTemplate: string = "Take the role of a personal travel assistant, and answer the following question in detail: {question}?"
+) {
     let answer = ''
 
-console.log({question})
+    const prompt = PromptTemplate.fromTemplate(
+        promptTemplate
+    );
+
+    const formattedPrompt = await prompt.format({
+        question,
+    });
 
     try {
-        answer = await llm.invoke(question);
+        answer = await llm.invoke(formattedPrompt);
     } catch (e) {
         return 'Something went wrong'
     }
