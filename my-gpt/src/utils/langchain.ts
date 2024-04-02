1c37e9e5f4ed627b87ea1d1b1344f270bf761a46
Roy Derks <rmaeaderks@gmail.com>
2024-04-02 20:52:51 +0100

    Added ex7 solution

--- my-gpt/src/utils/langchain.ts
+++ my-gpt/src/utils/langchain.ts
@@ -1,33 +1,36 @@
-import { OpenAI } from "@langchain/openai";
-import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
-/*
-  You are a naming consultant for new companies.
-  What is a good name for a company that makes colorful socks?
-*/
-const llm = new OpenAI({
+const llm = new ChatOpenAI({
     openAIApiKey: import.meta.env.VITE_OPENAI_KEY,
     temperature: 1,
     modelName: "gpt-4-0125-preview", 
-    maxTokens: 300
 });
 
 export async function generateAnswer(
     question: string,
-    promptTemplate: string = "Take the role of a personal travel assistant, and answer the following question in detail: {question}?"
+    promptTemplate: string = "Take the role of a {role}, that answers questions in a {style} way.",
+    role: string = "Personal travel assistant",
+    style: string = "consistent" // What happens if you change this to detailed?
 ) {
     let answer = ''
 
-    const prompt = PromptTemplate.fromTemplate(
-        promptTemplate
-    );
+    const chatPrompt = ChatPromptTemplate.fromMessages([
+        ["System", promptTemplate],
+        ["User", "{question}"],
+    ])
 
-    const formattedPrompt = await prompt.format({
-        question,
+    const formattedPrompt = await chatPrompt.formatMessages({
+        role,
+        style,
+        question
     });
-
+    
     try {
-        answer = await llm.invoke(formattedPrompt);
+        const result = await llm.invoke(formattedPrompt);
+
+        answer = result?.content as string
+
     } catch (e) {
         return 'Something went wrong'
     }
